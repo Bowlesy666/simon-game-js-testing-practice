30ab7c5ee6c9dae037a06de053d317bfaa523a0f
David Bowles <david.bowles84272@outlook.com>
2023-08-08 20:34:18 +0000

    Initial Commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,6 @@
-beforeAll(() => {
-    let fs = require('fs');
-    let fileContents = fs.readFileSync('index.html', 'utf-8');
-document.open();
-document.write(fileContents);
-document.close();
-})
+let game = {
+    score: '',
+};
 
 
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,14 @@
 /**
  * @jest-environment jsdom
  */
+
+const game = require('../game');
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
